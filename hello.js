ad5d28990289a2c05fc0856fabff1dbb1788d790
bola <bfagbowore@gmail.com>
2016-05-21 23:32:33 +0100

    adding hide and show functionality to sections in the html file

--- hello.js
+++ hello.js
@@ -1,12 +1,30 @@
 $(document).ready(function() {
+	$('#other').hide();
     $.ajax({
-        url: "https://api.github.com/users/techbola?client_id=bef8ea7b242ccd6eea1c&client_secret=9397daa9a55fa06b819bd97305baf93752116888"
+        url: "https://api.github.com/users/techbola"
     }).then(function(data) {
        $('.greeting-id').append(data.id);
-       $('<img src="" />').append(data.avatar_url);
        $('.greeting-name').append(data.name);
        $('.greeting-location').append(data.location);
        $('.greeting-login').append(data.login);
     });
+
+    $( "#btn" ).click(function(e){
+        e.preventDefault();
+
+          $("#first").hide();
+          $("#other").show();
+
+      });
+    $( "#btn2" ).click(function(e){
+        e.preventDefault();
+
+          $("#first").show();
+          $("#other").hide();
+
+      });
+
+
+
 });
 
